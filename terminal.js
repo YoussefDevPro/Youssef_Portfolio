394e4552e093e367db09eeb10f84c5066145f549
Youssef lmouden <youssef.pro.coding@gmail.com>
2024-12-09 19:05:25 +0100

    upgrading the terminal >:]

--- terminal.js
+++ terminal.js
@@ -1,28 +1,33 @@
 let currentDir = '/home/user';
+let promptText = currentDir +'$ ';  // Texte pré-défini initial du prompt
+
+function scrollToBottom() {
+    window.scrollTo(0, document.body.scrollHeight); // Déplace de 100 pixels vers le bas à chaque appel
+  }
+
+
 
 document.getElementById('input').addEventListener('keypress', function (event) {
     if (event.key === 'Enter') {
         const inputText = event.target.value.trim();
         const output = document.getElementById('output');
         
-        // Add a newline before the input command to start it on a new line
-        output.innerHTML += `\n> ${inputText}\n`; 
+        // Ajoute une nouvelle ligne avant la commande
+        output.innerHTML += `\n\n${promptText}${inputText}\n`; 
         
         if (inputText === 'gizzy') {
             const img = document.createElement('img');
-            img.src = 'static/gizzy.png'; // Replace with your image URL
+            img.src = 'static/gizzy.png'; // Remplace par ton image
             img.style.width = '300px';
             img.style.marginTop = '10px';
-            output.appendChild(img); // Append the image without removing the previous content
-        }
-        else if (inputText === 'gizzy -fr') {
+            output.appendChild(img); // Ajoute l'image sans supprimer le contenu précédent
+        } else if (inputText === 'gizzy -fr') {
             const img = document.createElement('img');
-            img.src = 'static/gizzyfr.png'; // Replace with your image URL
+            img.src = 'static/gizzyfr.png'; // Remplace par ton image
             img.style.width = '300px';
             img.style.marginTop = '10px';
-            output.appendChild(img); // Append the image without removing the previous content
-        }
-         else if (inputText === 'kabir') {
+            output.appendChild(img); // Ajoute l'image sans supprimer le contenu précédent
+        } else if (inputText === 'kabir') {
             const sound = document.getElementById('kabir-sound');
             sound.play();
             output.innerHTML += '\nKabir sound playing...';
@@ -32,14 +37,14 @@ document.getElementById('input').addEventListener('keypress', function (event) {
             const newDir = inputText.split(' ')[1];
             if (newDir) {
                 currentDir = `/home/user/${newDir}`;
+                promptText = `${currentDir} $ `; // Mettre à jour le prompt pour afficher le nouveau répertoire
+                document.getElementById('input').setAttribute('placeholder', promptText); // Met à jour le texte pré-défini dans l'input
                 output.innerHTML += `\nChanged directory to ${currentDir}`;
             } else {
                 output.innerHTML += `\nPlease specify a directory.`;
             }
         } else if (inputText === 'ls') {
-            output.innerHTML += `
-Desktop  Documents  Downloads  Music  Pictures  Public  Videos
-            `;
+            output.innerHTML += `\nDesktop  Documents  Downloads  Music  Pictures  Public  Videos`;
         } else if (inputText === 'pwd') {
             output.innerHTML += `\n${currentDir}`;
         } else if (inputText === 'cat /etc/os-release') {
@@ -71,7 +76,7 @@ DESCRIPTION
         } else if (inputText === 'sudo') {
             output.innerHTML += `\nSudo command requires superuser privileges. Try running as root.`;
         } else if (inputText === 'clear') {
-            output.innerHTML = ''; // Clears the terminal output
+            output.innerHTML = ''; // Effacer le terminal
         } else if (inputText === 'help') {
             output.innerHTML += `
 Available commands:
@@ -91,8 +96,6 @@ Available commands:
   df       - Show disk space usage
   neofetch - Show system info (simulated)
   neofetch -r   - Run real neofetch (Linux systems only)
-  clear    - Clear the terminal output
-
             `;
         } else if (inputText === 'uptime') {
             output.innerHTML += `\nUp for 3 days, 12 hours, 45 minutes`;
@@ -104,71 +107,59 @@ Filesystem     1K-blocks    Used Available Use% Mounted on
             `;
         } else if (inputText === 'neofetch') {
             output.innerHTML += `
-⠛⠛⣿⣿⣿⣿⣿⡷⢶⣦⣶⣶⣤⣤⣤⣀⠀⠀⠀
-⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀
-⠀⠀⠀⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇⠀
-⠀⠀⢤⣀⣀⣀⠀⠀⢸⣷⡄⠀ ⣀⣤⣴⣿⣿⣿⣆
-⠀⠀⠀⠀⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠦⠴⢿⢿⣿⡿⠷⠀⣿⠀
-⠀⠀⠀⠀⠀⠀⠀⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃⠀
-⠀⠀⠀⠀⠀⠀⠀⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⠟
-
-OS: Trolix OS v1.0
-Kernel: 5.10.0-9-amd64
-Uptime: 3 days, 12 hours, 45 minutes
-Kernel: 5.x.x
-Uptime: 2 hours, 20 minutes
-Packages: 200+
-Shell: Bash
-Resolution: 1920x1080
-DE: TROLL
-WM: Mutter
-CPU: Troll Core T4600 @ 2.00GHz
-GPU: TROLL T2000
-RAM: 16GB
-
+OS: Trolix OS v1.0                   ⠛⠛⣿⣿⣿⣿⣿⡷⢶⣦⣶⣶⣤⣤⣤⣀⠀⠀⠀
+Kernel: 5.10.0-9-amd64               ⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀
+Uptime: 3 days, 12 hours, 45 minutes ⠀⠀⠀⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇⠀
+Packages: 200+                       ⠀⠀⢤⣀⣀⣀⠀⠀⢸⣷⡄⠀ ⣀⣤⣴⣿⣿⣿⣆
+Shell: Bash                          ⠀⠀⠀⠀⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿
+Resolution: 1920x1080                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟
+DE: TROLL                            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠦⠴⢿⢿⣿⡿⠷⠀⣿⠀
+WM: Mutter                           ⠀⠀⠀⠀⠀⠀⠀⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃⠀
+CPU: Troll Core T4600 @ 2.00GHz      ⠀⠀⠀⠀⠀⠀⠀⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
+GPU: TROLL T2000                     ⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀
+RAM: 16GB                            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⠟                           
             `;
         } else if (inputText === 'neofetch -r') {
-            // This requires 'neofetch' to be installed on your system
-            output.innerHTML += `\n
-⡀⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠉⠁⠀⣀⣀⠀⣀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⡀⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣛⣿⣿⣷⣿⣿⣯⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣏⠉⣿⠉⢹⠟⢉⠉⢻⣿⠉⢻⠋⠙⡏⣿⠋⢻⡏⠉⣿⠉⣉⣻⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣻⡀⠈⢀⣿⠀⢸⠀⠀⣿⠀⢸⠀⠰⣿⣿⠀⢸⠁⢀⡟⠀⢹⣿⠀
-⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⡿⠿⠿⢿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣧⠀⣼⣿⠀⢸⡀⠀⣏⠀⢸⠀⠀⣿⣿⡄⠘⠀⢸⡇⠀⢰⣾⠀
-⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⡿⠋⠀⠀⠀⠀⠀⠈⠉⠉⠁⠀⠀⠈⢻⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣀⣿⣿⣆⡈⢁⣰⣿⣄⠘⢀⣼⣿⣿⣇⣀⣀⣼⣧⣀⣈⣹⡇
-⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⣿⣿⣿⣿⣿⣿⠟⠿⢿⣿⠿⠛⠛⠻⠿⠿⠻⠛⠉⠉⠉⠀
-⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀⢀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣧⣄⠀⠀⠀⠀⠀⠀⣴⠶⡶⠿⣿⣿⠿⠿⢿⡿⠿⠿⣿⠿⢿⡿⢿⡿⠀⠀⠀⠀⠀⠀⠀
-⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⣿⣿⠀⠀⢨⣭⣽⣿⡇⠀⢠⣾⣿⣿⣷⣆⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀⣿⠀⢱⡆⠈⣿⠀⢴⣾⡇⠀⣶⣿⠀⠘⡇⠀⡇⠀⠀⠀⠀⠀⠀⠀
-⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⠉⠛⠀⠀⠀⠀⠉⠁⠀⠀⠘⡏⠉⠉⠛⠋⠀⣠⣼⣿⠀⠀⠀⠀⠀⠀⣿⠀⢨⡁⠺⣿⠀⣈⣹⡇⠀⣉⣿⠀⡀⠁⠀⡇⠀⠀⠀⠀⠀⠀⠀
-⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⢹⡄⠀⠀⠀⠀⣿⣿⡿⠀⠀⠀⠀⠀⠀⣿⠀⠸⠇⠀⣿⠀⠹⢿⡇⠀⠿⢿⠀⢸⡀⠀⡇⠀⠀⠀⠀⠀⠀⠀
-⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⢷⣄⡀⠀⢠⡾⠋⠀⠛⢶⣶⣾⡇⠀⣠⠄⢰⣿⠟⠀⠀⠀⠀⠀⠀⠀⠻⢶⣶⡶⠚⠓⠶⠶⠾⠷⠶⠶⠾⠶⠾⠳⠾⠟⠀⠀⠀⠀⠀⠀⠀
-⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣹⡷⣠⠏⠙⢷⣶⠲⠶⠶⣷⣶⡿⠋⢀⣾⠃⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣹⣧⡀⢀⠀⠀⣀⣀⣀⡀⢀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠟⣫⣽⠃⠀⠀⠀⠉⠉⠙⠛⠋⠀⠀⢀⣾⡃⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠉⢉⡉⠻⡏⠉⣿⠟⢉⡉⠙⣿⠉⢹⡏⢉⡿⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠛⠁⠀⣼⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⡏⢳⡄⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠸⠇⣰⡇⠀⣿⠀⢸⣧⣀⣿⠀⠈⠀⣼⠁⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⢀⣴⠏⠀⠀⠀⢸⣿⣿⡀⠀⠀⠰⣦⣄⡀⣀⣤⡾⣿⣿⣧⠀⠻⢦⣄⡀⠀⠀⠀⠀⠀⣿⠀⢸⠀⠈⡇⠀⣿⠀⢸⡟⠛⣿⠀⢠⠀⢹⣆⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠘⠁⠀⠀⠀⠀⣾⣿⣿⣷⣄⡀⠀⠙⠿⣿⣏⣽⣿⣿⣿⣿⠄⢸⣧⠈⠙⠶⣤⣀⠀⠀⣿⣀⣸⣄⣠⣷⣀⣿⣦⣀⣁⣠⣿⣀⣸⣧⣀⣿⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⠀⠀⠹⣆⠀⠀⠀⠉⠳⣦⡀⠉⠉⠙⠻⣿⠉⠁⠀⠉⠉⠀⠀⠈⠉⠀⠉⠹⠇⠀⠀⠀⠀⠀⠀
-⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⢿⡆⠀⠀⠀⠀⠻⣿⠓⠒⠲⢦⣹⠷⠒⠲⣶⡖⠒⣶⣶⠒⢶⣾⠗⠒⠲⡶⠒⡖⠶⣄
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⡞⣿⠀⠀⠀⠀⠀⢹⠀⢹⡀⢈⡏⠀⣿⠀⢸⡇⠀⣿⡟⠀⢸⣿⠀⢸⣶⡇⠀⢳⠀⢸
-⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀⢀⣰⠃⢹⡆⠀⠀⠀⠀⢸⠀⢠⠀⠛⡇⠀⣿⠀⢸⡇⠀⣿⡇⠀⢸⣿⠀⢠⣬⡇⠀⢸⠀⢸
-⠀⠀⠀⠀⠀⠀⠀⠙⠛⠛⠋⠉⠀⠺⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠶⠞⠋⠀⠀⠀⢿⠀⠀⠀⠀⣸⠀⢸⠀⢰⣧⠀⠛⠀⣸⡇⠀⠛⣧⠀⠘⢻⠀⠘⠛⡇⠀⠚⠀⢸
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠆⠀⠀⠀⠿⠓⠛⠓⠛⠉⠙⠒⠚⠉⠛⣛⡚⠛⠛⠛⠛⠛⠓⡚⠛⠛⠓⠛⠉
+            output.innerHTML += `
+⣿⣿⣿⠿⡿⠀⠀⠈⠙⣿⣿⡿⠁⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣷⣼⡇⢸⣟⣿⣿⣿⣿⣿⣿⣟⣵⣦⡀⠏⣿⡄⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢺
+⣿⣿⣇⣤⡇⠀⠀⠀⣴⣿⠿⠃⠀⣭⣿⣿⣿⣿⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⠉⢸⣿⡟⣇⣾⡗⠀⠀⠀⠨⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸
+⣿⣿⡏⡏⡅⠀⢀⣴⣿⣿⣿⣿⠶⢿⣽⣿⣿⣿⣿⣿⣿⣿⡿⠿⠟⠋⠉⠉⠉⠉⠉⠉⠙⠛⠋⠳⣼⣽⣇⢿⡇⣿⡆⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣷⣅⣷⠀⣼⣿⣿⣿⡿⣿⣯⣽⣿⣿⣿⣿⣿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣿⣾⣿⣿⣧⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⢿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠀⣿⡿⣿⡄⣻⣧⠠⠴⡇⢸⣿⡗⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠸⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠘⣧⠹⣿⣿⡄⠀⠀⡇⢸⡛⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⡿⠟⠋⠀⠀⠀⠀⠀⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⡀⠀⠀⠀⠀⠸⠂⠘⣇⡀⠙⢿⡗⠀⠀⡷⢸⢱⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⡿⣿⠏⠁⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⠇⢀⣤⡶⠶⢿⡿⠿⠿⢦⡀⠸⣾⣿⣿⡿⠿⠷⢶⣤⣄⠀⢻⣧⠀⡾⠁⠀⠀⡇⢸⢄⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠
+⣿⣿⡟⠇⠿⠉⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⠀⡟⠀⣡⣴⣶⣶⣧⠈⠁⠀⠀⠈⠙⠿⠾⠷⠶⣦⣄⣹⡆⠀⣿⣾⠇⠀⠀⠀⡟⢸⢀⠟⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⠀⠿
+⣿⣿⡇⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⠀⠁⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⡇⠀⣧⣟⣀⡀⠀⠀⣷⣀⣘⣧⣤⣤⣶⣶⣶⣿⣿⣿⣿⣿⡉⠉⠀⠀⠀
+⣿⣿⡇⠅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠐⠀⠀⠐⣆⡀⠀⠀⠀⠀⠀⠀⠇⠀⣿⠙⢿⣽⣟⠋⠛⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠚⠉⠀
+⣿⣿⡇⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡟⠙⣷⠀⠀⠀⠀⠀⠀⠀⣠⡶⣶⣦⣷⣭⣿⡄⠀⠀⠀⠀⠀⡎⠀⢿⣇⢸⣷⡿⠶⢶⡶⢾⠛⠋⠙⠋⠉⣨⣿⣦⣽⢧⡀⠀⠀⠀⠀⠀
+⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣧⣼⠏⠀⠀⠀⠀⠀⠀⠀⠉⠛⠛⠋⠉⠁⠀⠙⠀⠀⠀⠀⠀⢿⠀⣨⠿⣿⠁⠀⣀⡞⠀⠘⣆⣠⠞⢀⣴⣿⣿⣿⣿⣿⣝⣇⠀⠀⠀⠀
+⣿⣿⡇⠋⠀⠀⣀⣀⣤⠤⠄⠀⠀⠀⠀⣿⢇⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⣽⡁⢀⡿⠀⠀⠈⢷⠀⢀⡟⠀⢀⣾⣿⣿⣿⠟⠻⣿⣿⣿⠳⣄⣀⢀
+⣿⣿⠇⣼⣿⣿⡿⣿⡻⣲⢶⠄⠀⠀⠀⣿⣿⣆⡠⠀⠀⠀⠀⠀⣴⣿⣿⣭⣭⣭⣿⣷⡦⠀⠀⠀⠀⠀⢠⢿⣿⣿⣦⣥⣄⡀⢸⣀⣤⣀⣴⣿⣿⣿⡟⠃⠀⠀⠈⠻⣿⣷⣈⠉⠉
+⣿⣿⠰⡇⠐⠈⠀⠋⣩⡽⣿⠀⠠⣄⠰⣿⣟⣿⠟⢧⠀⠀⠀⠀⠛⢿⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀⢠⡾⡏⠀⠟⣿⣿⣿⣿⠛⢻⣿⣿⣿⡿⢻⠏⠀⠀⠀⠀⠀⠈⣿⣿⣿⣆⠀
+⣿⣿⢸⠇⠀⢴⣾⣿⣖⡯⣿⡇⠀⢮⠉⣿⣎⣋⣠⣾⡁⠀⠀⠀⠀⠀⠻⢤⣤⠞⡁⠀⠀⠀⠀⠀⢠⠿⣼⢿⡛⡄⢸⣿⣿⡿⠀⣿⣿⣿⣿⣿⣷⣶⠴⢶⣄⣀⣤⠌⠿⠛⠛⢻⡄
+⣿⡿⢸⠀⠀⠉⣻⣿⢵⣻⣿⠀⠀⣟⠷⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠲⣶⣶⡞⠃⠀⠀⠀⠀⠀⠀⠀⢻⡊⢳⣶⡛⠿⠿⣷⡤⣾⣿⣿⣿⠏⠉⠉⠠⣤⡌⠉⠀⠀⠀⠀⠀⠀⠱
+⣿⡇⣿⠀⠰⣿⣿⣿⡽⣾⣿⠀⠰⢯⣠⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡇⢸⣿⣿⣦⣄⠈⣧⢘⣿⣿⡇⠀⢀⣀⣤⣤⡤⠀⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⠀⢈⣺⣿⣿⣯⣿⡇⠀⢘⠳⣿⣿⣿⣿⣿⠿⠍⠿⣿⠀⠘⠲⣤⣄⣀⣀⣀⣀⣠⠾⠃⠀⠀⢀⡞⠀⣾⣿⣿⣿⣿⣷⣝⣃⡿⢿⠇⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⠀⣸⣿⣿⣿⣺⢽⠇⠀⢨⣹⣿⣿⣿⣿⣯⣠⣤⣴⢛⡇⠀⠀⠈⠙⢿⣿⠟⠉⠀⠀⠀⠀⣠⠟⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣶⣶⣶⣤⣤⣤⣀⣀⠀⠀⠀
+⣿⣿⣿⠁⣽⢟⣿⡟⠋⠛⠀⠀⠓⢬⣿⣿⣿⣿⡏⠀⡼⠁⣼⡇⠀⠀⠀⠀⠀⢶⡄⠀⠀⠀⢀⡴⠃⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶
+⣿⣿⣿⠁⣤⣿⣿⣷⡶⣶⣤⣄⣀⣸⣾⣿⣿⣿⡇⢸⠇⠀⣿⡇⠀⠀⠀⠀⠀⠙⠟⠀⢀⡴⠋⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⡖⠸⢿⡍⣼⣹⡟⡉⠹⡍⠁⢸⣿⣿⣿⣷⣿⠀⠀⢿⣷⠀⠀⠀⠀⠀⠀⢈⣷⠟⠁⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⡇⠁⠈⡟⠿⢻⠘⠶⠄⡇⠀⢸⣿⣿⣿⣿⣿⠀⠀⠀⡻⣦⠀⠀⠀⢀⣴⠿⠋⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿            
             `;
-            // If using Node.js or server-side, you'd execute the command here
-            // For now, we simulate that it's running
-        } else if (inputText === 'clear') {
-            output.innerHTML = 'Welcome to the terminal. Type a command.'; // Clears the terminal output
         } else {
             output.innerHTML += `\nCommand not found: ${inputText}`;
         }
 
-        // Scroll to the bottom of the terminal for new output
-        output.scrollTop = output.scrollHeight;
+        // Faire défiler vers le bas à chaque nouvelle ligne
+        scrollToBottom()
         
-        // Clear the input field for the next command
+        
+        // Réinitialiser l'entrée après la commande
         event.target.value = '';
+        scrollToBottom()
     }
 });
+
+// Initialiser le texte pré-défini de l'input
+document.getElementById('input').setAttribute('placeholder', promptText);
